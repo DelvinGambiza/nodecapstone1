8664e0ed32b1ed8eecd1906ab4f143ba3ec12272
Adem Mohammed <45444428+abulhuman@users.noreply.github.com>
2022-04-27 00:54:34 +0300

    Update server.js
    Added `/sys` route
    
    It saves information about the os in a file named `osinfo.json` with the status code set to 201, the content type to text/plain with the message "Your OS info has been saved successfully!" sent back to the user. The `osinfo.json` file will contain the following attributes:
    
    ```
    hostname: the os host name,
    platform: the os platform,
    architecture: the os architecture,
    numberOfCPUS: the os number of cores,
    networkInterfaces: os network interfaces,
    uptime: os uptime
    ```

--- server.js
+++ server.js
@@ -1,9 +1,28 @@
 const express = require('express')
+const fs = require('node:fs')
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
   res.render('/index.html')
+})
+
+app.get('/sys', (req, res) => {
+   const osInfo = {
+     hostname: os.hostname(),
+     platform: os.platform(),
+     architecture: os.arch(),
+     numberOfCPUS: os.cpus().length,
+      networkInterfaces: os.networkInterfaces(),
+     uptime: os.uptime()
+   }
+   const osInfoJSON = JSON.stringify(osInfo)
+   
+   fs.writeFile('osinfo.json', osInfoJSON, (err) => {
+     if(err) throw err;
+     res.set('Content-Type', 'text/html')
+     res.status(201).send('Your OS info has been saved successfully!')     
+   })
 })
 
 app.listen(port, () => {
